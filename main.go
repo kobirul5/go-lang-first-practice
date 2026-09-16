1132dbf67a3202cc925887b23ff349714a57e931
Md. Kobirul Islam <kobirul05j@gmail.com>
2026-09-16 22:55:49 +0600

    Refactor main function to print name and age variables

--- main.go
+++ main.go
@@ -7,9 +7,16 @@ func main(){
 	// fmt.Println("Hello, World!")
 
 	// var name string = "Kobirul"
-	name:= "kobirul 2"
+	// name:= "kobirul 2"
+
+	//  grouped variables
+	
+	var (
+		name string ="liton"
+		age int =5
+	)
 	
 	
 
-	fmt.Println(name)
+	fmt.Println(name, age)
 }
