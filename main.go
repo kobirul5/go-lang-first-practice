982ff2fe6d08f405c85b323dd5166cd2e5e6740f
Md. Kobirul Islam <kobirul05j@gmail.com>
2026-09-16 23:00:03 +0600

    Remove commented-out variable declarations and clean up main function

--- main.go
+++ main.go
@@ -7,16 +7,22 @@ func main(){
 	// fmt.Println("Hello, World!")
 
 	// var name string = "Kobirul"
-	// name:= "kobirul 2"
+	// name:= "kobirul 2"  // short way , most used in real project
 
 	//  grouped variables
-	
-	var (
-		name string ="liton"
-		age int =5
-	)
-	
-	
+	// var (
+	// 	name string ="liton"
+	// 	age int =5
+	// )
+	// fmt.Println(name, age)
 
-	fmt.Println(name, age)
+	// multiple variable declaration
+	var x, y int
+	 x = 20
+	 y = 30
+	 fmt.Println(y-x)
+	 
+	 var a, b string = "Ph", "hero"
+	 fmt.Println(a+b)
+
 }
