ed7b57e03cf8d373ed8e63bb01229fbd95010480
Md. Kobirul Islam <kobirul05j@gmail.com>
2026-09-16 22:53:04 +0600

    Update main function to print variable name instead of greeting

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello, World!")
+	
+	// fmt.Println("Hello, World!")
+
+	// var name string = "Kobirul"
+	name:= "kobirul 2"
+	
+	
+
+	fmt.Println(name)
 }
