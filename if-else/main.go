8780a00c0295726fab61b315651557252b2c6b7f
Md. Kobirul Islam <kobirul05j@gmail.com>
2026-09-17 23:20:54 +0600

    Add if-else scope example to main function

--- if-else/main.go
+++ if-else/main.go
@@ -13,4 +13,20 @@ func main() {
 		fmt.Println("You are a senior citizen.")
 	}
 
+	// if-else scope example
+
+
+	if score:=50; score >= 90 { // if-else scope example
+		fmt.Println("You got an A!")
+	} else if score >= 80 {
+		fmt.Println("You got a B!")
+	} else if score >= 70 {
+		fmt.Println("You got a C!")
+	} else if score >= 60 {
+		fmt.Println("You got a D!")
+	} else {
+		fmt.Println("You failed.")
+	}
+
+
 }
