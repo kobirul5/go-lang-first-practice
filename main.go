0a23da42f9278a78bfd42e48a611e0fbd16e1495
Md. Kobirul Islam <kobirul05j@gmail.com>
2026-09-17 22:59:02 +0600

    Comment out unused code and clean up main function

--- main.go
+++ main.go
@@ -1,58 +1,82 @@
+// package main
+
+// import "fmt"
+
+// //  isAdmin := true // wrong way, because short way not working out of main function
+
+
+// func makeCoffee(kind string){
+// 	fmt.Printf("maing %s coffee...\n" , kind)
+// }
+
+// func main(){
+	
+// 	// fmt.Println("Hello, World!")
+
+// 	// var name string = "Kobirul"
+// 	// name:= "kobirul 2"  // short way , most used in real project
+
+// 	//  grouped variables
+// 	// var (
+// 	// 	name string ="liton"
+// 	// 	age int =5
+// 	// )
+// 	// fmt.Println(name, age)
+
+// 	// multiple variable declaration
+// 	// var x, y int
+// 	//  x = 20
+// 	//  y = 30
+// 	//  fmt.Println(y-x)
+	 
+// 	//  var a, b string = "Ph", "hero"
+// 	//  fmt.Println(a+b)
+
+
+// 	// constant
+// 	// const pi = 3.14
+// 	//  fmt.Println(pi)
+
+
+// 	// var age int
+// 	// fmt.Println(age)//0
+
+
+// 	// var name string
+// 	// fmt.Println(name)//""
+
+// 	// var isAdmin bool
+// 	// fmt.Println(isAdmin)//false
+
+// 	// var score float64
+// 	// fmt.Println(score)//false
+
+// 	//basic function
+// 	makeCoffee("black")
+// 	makeCoffee("cold")
+
+// }
+
+
+// Module 2 start
+
 package main
 
 import "fmt"
 
-//  isAdmin := true // wrong way, because short way not working out of main function
+func main() {
 
+	// anonymous function
+	coffee := func() {
+		fmt.Println("I like coffee")
+	}
 
-func makeCoffee(kind string){
-	fmt.Printf("maing %s coffee...\n" , kind)
-}
+	// calling the anonymous function
+	coffee()
 
-func main(){
-	
-	// fmt.Println("Hello, World!")
-
-	// var name string = "Kobirul"
-	// name:= "kobirul 2"  // short way , most used in real project
-
-	//  grouped variables
-	// var (
-	// 	name string ="liton"
-	// 	age int =5
-	// )
-	// fmt.Println(name, age)
-
-	// multiple variable declaration
-	// var x, y int
-	//  x = 20
-	//  y = 30
-	//  fmt.Println(y-x)
-	 
-	//  var a, b string = "Ph", "hero"
-	//  fmt.Println(a+b)
-
-
-	// constant
-	// const pi = 3.14
-	//  fmt.Println(pi)
-
-
-	// var age int
-	// fmt.Println(age)//0
-
-
-	// var name string
-	// fmt.Println(name)//""
-
-	// var isAdmin bool
-	// fmt.Println(isAdmin)//false
-
-	// var score float64
-	// fmt.Println(score)//false
-
-	//basic function
-	makeCoffee("black")
-	makeCoffee("cold")
+	//immediately invoked function expression (IIFE)
+	func(coffeeType string) {
+		fmt.Println("I like " + coffeeType)
+	}("tea")
 
 }
