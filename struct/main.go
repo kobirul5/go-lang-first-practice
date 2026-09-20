de9d16e9bbf0f17be987b6e7c0902f578c09d616
Md. Kobirul Islam <kobirul05j@gmail.com>
2026-09-20 16:59:15 +0600

    add initial implementation of Person struct and main function

---
+++ struct/main.go
@@ -0,0 +1,31 @@
+package main
+
+
+import "fmt"
+
+type additionalInformation struct {
+	Address string
+	Phone   string
+}
+
+type Person struct {
+	Name string
+	Age  int
+	Info additionalInformation
+}
+
+func main() {
+	p1 := Person{Name: "Alice", Age: 30}
+	p2 := Person{Name: "Bob", Age: 25}
+
+	fmt.Println("Person 1:", p1)
+	fmt.Println("Person 2:", p2)
+
+	// Accessing fields
+	fmt.Println("Name of Person 1:", p1.Name)
+	fmt.Println("Age of Person 2:", p2.Age)
+
+	// Modifying fields
+	p1.Age = 31
+	fmt.Println("Updated Age of Person 1:", p1.Age)
+}
