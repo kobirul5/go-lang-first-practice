f0f2d8ed31db0ad7b6f7cbc437445501a251de4d
Md. Kobirul Islam <kobirul05j@gmail.com>
2026-09-21 19:00:47 +0600

    add range usage examples with arrays and slices

--- range/main.go
+++ range/main.go
@@ -10,4 +10,15 @@ func main() {
 	for key, value := range myMap {
 		println(key, ":", value)
 	}
+
+	myArr := []int{1, 2, 3, 4, 5}
+	for index, value := range myArr {
+		println("Index:", index, "Value:", value)
+	}
+
+	// Using range with a slice
+	for i, v := range myArr {
+		println("Index:", i, "Value:", v)
+	}
+
 }
