db033a1d419f72d0717ae9ad5cde5559acc7f370
Md. Kobirul Islam <kobirul05j@gmail.com>
2026-09-21 19:23:47 +0600

    add initial implementation of animal interface with Dog and Cat structs

---
+++ interface/main.go
@@ -0,0 +1,27 @@
+package main
+
+
+type animal interface {
+	Speak() string
+}
+
+
+type Dog struct {}
+type Cat struct {}
+
+func (d Dog) Speak() string {
+	return "Woof!"
+}
+
+func (c Cat) Speak() string {
+	return "Meow!"
+}
+
+
+func main() {
+	var myDog Dog
+	var myCat Cat
+
+	println(myDog.Speak())
+	println(myCat.Speak())
+}
