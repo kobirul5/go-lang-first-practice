b44181348f42bddff6842354e9d5e72a3319b496
Md. Kobirul Islam <kobirul05j@gmail.com>
2026-09-24 01:05:50 +0600

    use enum

---
+++ enum/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type DayOfWeek int
+
+const (
+	Monday DayOfWeek = iota
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+	Sunday
+)
+
+func getWorkDayStatus(day DayOfWeek) string {
+	switch day {
+	case Monday, Tuesday, Wednesday, Thursday, Friday:
+		return "Workday"
+	case Saturday, Sunday:
+		return "Weekend"
+	default:
+		return "Invalid day"
+	}
+
+}
+
+func main() {
+	fmt.Println(getWorkDayStatus(Monday)) // Output: Workday
+}
