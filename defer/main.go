c8e8f2caae0e4bc2504b6a21aa5b0ec6dc135a96
Md. Kobirul Islam <kobirul05j@gmail.com>
2026-09-24 00:58:09 +0600

    add namedDefer function with deferred result handling

--- defer/main.go
+++ defer/main.go
@@ -14,17 +14,38 @@ func exampleDefer() int{
 	defer deferredFunction(result)
 	fmt.Println("I am from exampleDefer function and result is ", result)
 
+	// multiply deferred 
+	defer fmt.Println("I am from exampleDefer function and result is ",1)
+	defer fmt.Println("I am from exampleDefer function and result is ",2)
+	defer fmt.Println("I am from exampleDefer function and result is ",3)
+	defer fmt.Println("I am from exampleDefer function and result is ",4)
+	defer fmt.Println("I am from exampleDefer function and result is ",5)
 	result += 20
 
 	return result 
 
 }
 
+func namedDefer() (result int) {
+	result = 10
+
+ defer func() {
+	result += 20
+        fmt.Println("I am from deferred function and result is", result)
+    }()
+
+
+	fmt.Println("I am from namedDefer function and result is ", result)
+	result += 20
+	return
+}
 
 func main() {
 	defer fmt.Println("I am from Deferred print call ")
 	fmt.Println("I am from main function")
 
+
 	exampleDefer()
+	namedDefer()
 
 }
