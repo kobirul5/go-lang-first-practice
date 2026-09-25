38d6a2745e39137c4b387fa9d2ce42aaaa4c67a7
Md. Kobirul Islam <kobirul05j@gmail.com>
2026-09-25 21:23:25 +0600

    implement concurrency example with file upload, database save, and email sending

---
+++ concurrency/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+
+	var startTime = time.Now()
+
+	// concurrency
+	uploadFile()
+	saveToDb()
+	sendEmail()
+
+	fmt.Println("all task completed ")
+	fmt.Println("Total time taken: ", time.Since(startTime))
+}
+
+func uploadFile() {
+	// code to upload file
+	fmt.Println("uploading file")
+	time.Sleep(3 * time.Second)
+	fmt.Println("file upload done ")
+}
+
+func saveToDb() {
+	// code to save to database
+	fmt.Println("saving to database")
+	time.Sleep(3 * time.Second)
+	fmt.Println("data saved to database ")
+}
+func sendEmail() {
+	// code to send email
+	fmt.Println("sending email")
+	time.Sleep(3 * time.Second)
+	fmt.Println("email sent ")
+}
